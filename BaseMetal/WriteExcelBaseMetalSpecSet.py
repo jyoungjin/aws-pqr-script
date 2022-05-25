01d1c1bfc791ef30200ab189e04a134a4466037a
jyoungjin <ccd1202@naver.com>
2022-05-25 17:17:27 +0900

    2022.05.25(수) 작업내역

--- BaseMetal/WriteExcelBaseMetalSpecSet.py
+++ BaseMetal/WriteExcelBaseMetalSpecSet.py
@@ -1,8 +1,11 @@
 import json
 import os
+from openpyxl import Workbook
 
 dir_path = "/Users/youngjin/workspace/json-data/aws-pqr"
 counts = dict()
+
+write_wb = Workbook()
 
 for(root, directories, files) in os.walk(dir_path):
     # file 순회
@@ -18,14 +21,18 @@ for(root, directories, files) in os.walk(dir_path):
                     type_and_grade = jsonData['base_metals']['type_and_grade']
                     to_type_and_grade = jsonData['base_metals']['to_type_and_grade']
 
-                    key = "spec: "+material_spec+", type:"+type_and_grade+""
-                    to_key = "spec: "+to_material_spec+", type:"+to_type_and_grade+""
+                    key = material_spec+","+type_and_grade
+                    to_key = to_material_spec+","+to_type_and_grade
                     counts[key] = counts.get(key, 0) + 1
                     counts[to_key] = counts.get(to_key, 0) + 1
 
-for item in counts.items():
-    print(item)
+write_ws = write_wb.active
+write_ws['A1'] = 'material_spec'
+write_ws['B1'] = 'type_and_grade'
+write_ws['C1'] = 'count'
 
-print(len(counts))
-# for item in counts.keys():
-#     print(item, end=', ')
+for key, val in counts.items():
+    arr = key.split(',')
+    write_ws.append([arr[0], arr[1], val])
+
+write_wb.save('/Users/youngjin/Desktop/test/test.xlsx')
